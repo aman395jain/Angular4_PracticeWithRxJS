e3e11003cd74338c9781be86227a8e2dad5fd0ec
aman395jain <er.amanjain395@gmail.com>
2018-06-18 01:03:57 +0530

    committed by Aman(18/06)

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
