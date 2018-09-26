347d8a98913a8d9ed4f2d9a4d7c80e25d084a28a
aman395jain <er.amanjain395@gmail.com>
2018-09-26 23:48:09 +0530

    committed by Aman(26/09)

--- app/user-descripton/user-descripton.component.ts
+++ app/user-descripton/user-descripton.component.ts
@@ -1,20 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {FlagOnUserDescriptionService} from "../app/service/flag-on-user-description.service";
 
 @Component({
     moduleId: module.id,
     selector: 'app-user-descripton',
     templateUrl: './user-descripton.component.html',
-    styleUrls: ['./user-descripton.component.css']
+    styleUrls: ['./user-descripton.component.css'],
+    providers: [FlagOnUserDescriptionService]
 })
-export class UserDescriptonComponent implements OnInit {
+export class UserDescriptonComponent {
 
-  constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+
+    private IsHidden: boolean = true;
+     // @ViewChild(UserDescriptionNestedComponent) userDescriptionNested;
+
+    constructor(private router: Router, private flagOnUserDescriptionService:FlagOnUserDescriptionService) {
+
+    }
+
+
+    ngAfterViewChecked() {
+         this.IsHidden = this.flagOnUserDescriptionService.getIsHidden()
+        console.log(this.flagOnUserDescriptionService.getIsHidden())
+    }
     navigateToUserNestedComp = () => {
-      this.router.navigate(['/userDiscription/userDiscriptionNested'])
+        this.IsHidden = false
+        this.router.navigate(['/userDiscription/userDiscriptionNested'])
+
     }
 
 }
